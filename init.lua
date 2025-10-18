d3b3ebb8f88a8b6345222d1228a51e5b9dbfc718
Giselle <cryponiess@gmail.com>
2025-10-18 13:12:37 -0700

    add stuff

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Bootstrap lazy.nvim
 require("config.lazy")
-
--- Load core config
+require("config.plugins")
 require("config.options")
 require("config.keymaps")
---
+++ lua/config/plugins.lua
@@ -0,0 +1,10 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('andweeb/presence.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-treesitter/nvim-treesitter')
+
+vim.call('plug#end')
+
