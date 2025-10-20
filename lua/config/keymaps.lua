af2a834a56256d3ca9488876243957e7f98699e3
Giselle <cryponiess@gmail.com>
2025-10-20 14:54:10 -0700

    update telescope config and more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,16 @@
+-- Scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Terminal keymaps
 
 vim.keymap.set("n", "<leader>th", ":vsplit | terminal<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>tv", ":split | terminal<CR>", { noremap = true, silent = true })
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+
+-- Comment out
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
+vim.keymap.set("v", "<C-_>", "gc", { remap = true })
 
 -- Move lines up and down
 vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move line up (Normal mode)" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,20 @@ return {
 		local builtin = require("telescope.builtin")
 
 		-- Setup telescope ONCE with all configurations
-		telescope.setup({})
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<CR>"] = require("telescope.actions").select_tab,
+					},
+					n = {
+						["<CR>"] = require("telescope.actions").select_tab,
+					},
+				},
+			},
+		})
 		-- Keymaps
 		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 	end,
 }
